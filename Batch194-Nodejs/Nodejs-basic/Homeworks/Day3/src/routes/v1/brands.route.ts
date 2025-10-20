12dd97034dabae79d420b74ab7c916c9212a6681
michealkun96 <michealkun96@gmail.com>
2025-10-20 10:23:43 +0700

    Homeworks-Day04

--- Batch194-Nodejs/Nodejs-basic/Homeworks/Day3/src/routes/v1/brands.route.ts
+++ Batch194-Nodejs/Nodejs-basic/Homeworks/Day3/src/routes/v1/brands.route.ts
@@ -6,18 +6,18 @@ const router = express.Router() as Router;
 
 
 
-//GET api/v1/categories ==> get all categories
+//GET api/v1/brands ==> get all brands
 router.get('/', brandsControllers.findAll)
-//GET api/v1/categories/:id ==> get category by id
+//GET api/v1/brands/:id ==> get brand by id
 router.get('/:id', brandsControllers.findById)
 
-//POST api/v1/categories ==> create new category
+//POST api/v1/brands ==> create new brand
 router.post('/', brandsControllers.create)
 
-//PUT api/v1/categories/:id ==> Update a category
+//PUT api/v1/brands/:id ==> Update a brand
 router.put('/:id', brandsControllers.updateById)
 
-//DELETE api/v1/categories/:id ==> Delete a category by id
+//DELETE api/v1/brands/:id ==> Delete a brand by id
 router.delete('/:id', brandsControllers.deleteById)
 
 export default router;
